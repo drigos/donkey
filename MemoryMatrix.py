0e30531d608384908d1c8529a33d6d9126870c4c
Rodrigo F Martins <martins.drg@gmail.com>
2014-09-07 12:25:02 -0300

    Implementado tamanho dinâmico dos tiles para preencher a tela

--- MemoryMatrix.py
+++ MemoryMatrix.py
@@ -5,8 +5,12 @@ from pygame.locals import *
 
 pygame.init()
 
+## Capturar dimensões da tela em fullscreen
+HEIGHT = 1000
+WIDTH  = 1000
+
 # Cria a janela
-DISPLAYSURF = pygame.display.set_mode((1000, 1000), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((HEIGHT, WIDTH), 0, 32)
 pygame.display.set_caption('Memory Matrix')
 
 # Define as cores
@@ -17,28 +21,66 @@ RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 
-TILE_SIZE = 48
-TILE_INTERSPACE = 4
-BOARD_BORDER = 8
-MIN_TILES = 3
-MAX_TILES = 18
-
-WINDOW_BORDER = 26
-
 # Preenche o surface com a cor especificada
 DISPLAYSURF.fill(WHITE)
 
-# Gera um número aleatório para ser o número de tiles e desenha o board
-tilesAmount = random.randint(MIN_TILES, MAX_TILES)
-boardSize = TILE_SIZE*tilesAmount + TILE_INTERSPACE*(tilesAmount-1) + 2*BOARD_BORDER
-pygame.draw.rect(DISPLAYSURF, BLACK, (WINDOW_BORDER, WINDOW_BORDER, boardSize, boardSize))
+# Define o tamanho do cabeçalho e o padding da janela e a borda da matriz
+HEADER = 100
+PADDING = min(HEIGHT-HEADER, WIDTH) * 0.03
+BORDER = min(HEIGHT-HEADER, WIDTH) * 0.01
+
+# Define as dimensões úteis para o desenho da matriz
+BODY_HEIGHT = HEIGHT - HEADER - 2*(PADDING + BORDER)
+BODY_WIDTH = WIDTH - 2*(PADDING + BORDER)
+
+# Define o espaço entre os tiles em função do tamanho do tile
+## Usar também o número de tiles para essa decisão
+## Caso contrário quando houver poucos tiles a borda irá ficar fina ou quando houver muitos a borda irá ficar grossa
+TILE_INTERSPACE_FACTOR = 0.1
+
+## Calcular nível
+level = 3
+## A partir do nível definir o número de tiles (e.g. 3x4)
+numTilesLower = 2     # Este deve ser sempre o menor lado
+numTilesGreater = 3   # Este deve ser sempre o maior lado
+
+# Se a divisão da largura da tela pela altura retornar um número maior que um
+# então a tela tem orientação horizontal, caso contrário orientação vertical
+if (BODY_WIDTH / BODY_HEIGHT <= 1): # Orientação horizontal
+   # Nesse caso a maior dimensão (núm. tiles) da matriz ficará na largura
+   numTilesHeight = numTilesLower
+   numTilesWidth = numTilesGreater
+else:                               # Orientação vertical
+   # Nesse caso a maior dimensão (núm. tiles) da matriz ficará na altura
+   numTilesHeight = numTilesGreater
+   numTilesWidth = numTilesLower
+
+# A fórmula usada aqui é uma fatoração da seguinte fórmula
+# BODY_HEIGHT == numTiles*tileSize + (numTiles - 1)*tileInterspace
+# Onde o tileInterspace está em função do tamanho do tile que é ## Usar também o número de tiles para essa decisão
+# BODY_HEIGHT == numTiles*tileSize + (numTiles - 1)*(tileSize*TILE_INTERSPACE_FACTOR)
+tileSizeH = BODY_HEIGHT / (numTilesHeight + (numTilesHeight - 1)*TILE_INTERSPACE_FACTOR)
+tileSizeW = BODY_WIDTH / (numTilesWidth + (numTilesWidth - 1)*TILE_INTERSPACE_FACTOR)
+# Como número de tiles pode variar no eixo X e Y, deve-se fazer os dois cálculo e usar o menor
+# Isso é demonstrado nos testes de mesa 1 e 2
+tileSize = min(tileSizeH, tileSizeW)
+
+# Aqui é dado um valor em pixel para o tileInterspace
+## Usar também o número de tiles para essa decisão
+tileInterspace = tileSize*TILE_INTERSPACE_FACTOR
+
+# Desenha o board
+boardSizeHeight = tileSize*numTilesHeight + tileInterspace*(numTilesHeight-1) + 2*BORDER
+boardSizeWidth = tileSize*numTilesWidth + tileInterspace*(numTilesWidth-1) + 2*BORDER
+## Centralizar o board
+pygame.draw.rect(DISPLAYSURF, BLACK, (PADDING, PADDING, boardSizeWidth, boardSizeHeight))
 
 # Desenha os tiles
-for i in range(0, tilesAmount):
-   posX = i*(TILE_SIZE+TILE_INTERSPACE) + BOARD_BORDER + WINDOW_BORDER
-   for j in range(0, tilesAmount):
-      posY = j*(TILE_SIZE+TILE_INTERSPACE) + BOARD_BORDER + WINDOW_BORDER
-      pygame.draw.rect(DISPLAYSURF, GRAY, (posX, posY, TILE_SIZE, TILE_SIZE))
+for i in range(0, numTilesWidth):
+   posX = i*(tileSize+tileInterspace) + BORDER + PADDING
+   for j in range(0, numTilesHeight):
+      posY = j*(tileSize+tileInterspace) + BORDER + PADDING
+      pygame.draw.rect(DISPLAYSURF, GRAY, (posX, posY, tileSize, tileSize))
 
 # Executa o game loop
 while True:
